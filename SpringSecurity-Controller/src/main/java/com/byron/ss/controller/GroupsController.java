7d26bee7dda43fb098835c9b3c4cbc8e797f1837
Administrator <Administrator@192.168.1.42>
2017-12-23 12:41:13 +0800

    change auth Controller

--- SpringSecurity-Controller/src/main/java/com/byron/ss/controller/GroupsController.java
+++ SpringSecurity-Controller/src/main/java/com/byron/ss/controller/GroupsController.java
@@ -1,9 +1,4 @@
-/*
- * Powered By [rapid-framework]
- * Web Site: http://www.rapid-framework.org.cn
- * Google Code: http://code.google.com/p/rapid-framework/
- * Since 2008 - 2014
- */
+
 
 package com.byron.ss.controller;
 
@@ -25,7 +20,7 @@ import com.byron.ss.service.UsersGroupsManager;
 
 
 /**
- * @author badqiu email:badqiu(a)gmail.com
+ * @author byron
  * @version 1.0
  * @since 1.0
  */
